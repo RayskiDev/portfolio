34971023c44f55bdfb2abd286e2b2986bf1136b7
RayskiDev <134840765+RayskiDev@users.noreply.github.com>
2024-06-24 21:01:03 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
+let type, icon, title, text;
+
+
 window.addEventListener("scroll", function () {
   var header = this.document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY > 0);
 })
 
 
-function message(text) {
-  document.innerHTML = text;
+function createToast(type, icon, title, text) {
+  let newToast = document.createElement('div');
+  let notifications = document.querySelector('.notifications');
+
+  newToast.innerHTML = `
+  <div class="toast ${type}">
+    <i class="${icon}"></i>
+    <div class="toast-content">
+        <div class="title">${title}</div>
+        <span>${text}</span>
+    </div>
+    <i class="fa-solid fa-xmark" onclick="(this.parentElement).remove()"></i>
+  </div>`;
+  notifications.appendChild(newToast);
+
+  newToast.timeOut = setTimeout(
+    ()=>newToast.remove(), 5000
+  )
+
 }
 
 
@@ -18,12 +38,12 @@ tabs.forEach((tab, index) => {
     tabs.forEach(tab => { tab.classList.remove('active') });
     tab.classList.add('active');
 
-    var line = document.querySelector('.line')
+    var line = document.querySelector('.line');
     line.style.width = e.target.offsetWidth + "px";
     line.style.left = e.target.offsetLeft + "px";
 
     about_content.forEach(content => { content.classList.remove('active') });
-    about_content[index].classList.add('active')
+    about_content[index].classList.add('active');
   })
 });
 
@@ -42,7 +62,6 @@ function sendMail() {
 
     emailjs
     .send("service_70pgysm","template_h7hguso",params);
-    alert("Wiadomość pomyślnie wysłana.");
     document.getElementById("contactForm").reset();
 }
 
@@ -51,10 +70,20 @@ function isEmpty() {
   var email = document.getElementById('subject').value;
   var message = document.getElementById('subject').value;
 
+
   if (subject == "" || email == "" || message == "") {
-    alert("Name must be filled out");
+    type = "fail"; icon = "fa-solid fa-circle-exclamation"; title = "Błąd!"; text = "Proszę wypełnić wszystkie pola.";
+    createToast(type, icon, title, text);
     return false;
   } else {
     sendMail();
+    type = "success"; icon = "fa-solid fa-circle-check"; title = "Sukces!"; text = "Wiadomość pomyślnie wysłana.";
+    createToast(type, icon, title, text);
   }
+}
+
+
+function siema() {
+  type = "success"; icon = "fa-regular fa-circle-check"; title = "Sukces!"; text = "Wiadomość pomyślnie wysłana.";
+  createToast(type, icon, title, text);
 }
